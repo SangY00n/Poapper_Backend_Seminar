44932791db168bd7e104aeb58259b9819f6ffd92
SangY00n <dbs1367@postech.ac.kr>
2021-11-15 20:44:06 +0900

    week4 Homework commit

---
+++ week4/handle.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+let database = [];
+
+const server = http.createServer((req, res) => {
+    const url_parsed = req.url.split('/');
+    const method = url_parsed[1];
+
+    console.log(url_parsed);
+    console.log(database);
+
+    if (method == 'create') {
+        database.push(url_parsed[2]);
+        res.end();
+    } else if (method == 'read') {
+        console.log(database.length)
+        if (url_parsed[2] >= 0 && url_parsed[2] < database.length)
+            res.write(database[url_parsed[2]]);
+        else {
+            console.log("invalid index");
+            res.write('invaild index');
+        }
+        // 데이터가 push 되지 않은 index에 해당하는 값을 읽고자 할 경우, error가 발생할 수 있다.
+    } else if (method == 'update') {
+        if (url_parsed[2] >= 0 && url_parsed[2] < database.length)
+            database[url_parsed[2]] = url_parsed[3];
+        else {
+            console.log("invalid index");
+            res.write('invaild index');
+        }
+        // 데이터가 push 되지 않은 index에 해당하는 값을 업데이트 하고자 할 경우, error가 발생할 수 있다.
+        res.end();
+    } else if (method == 'delete') {
+        database.pop();
+    }
+    res.end();
+});
+
+server.listen(8080, () => {
+    console.log('8080 port');
+})
